1474de424e13b7da071209cef113fa3689b44be9
Sumit <sumitscience2017@gmail.com>
2023-07-31 14:21:16 +0530

    django task is done

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
-from django.contrib.auth import login,logout,authenticate
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 
 def loginView(request):
-    pass
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("index"))
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'authentication/login.html', {'form': form})
 
 def logoutView(request):
-    pass
+    logout(request)
+    return redirect('login')
 
 def registerView(request):
-    pass
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Optionally, automatically log in the newly registered user.
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("index"))
+            else:
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{field}: {error}")
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authentication/register.html', {'form': form})
--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Book(models.Model):
@@ -9,6 +10,13 @@ class Book(models.Model):
     description = models.TextField(null=True)
     mrp = models.PositiveIntegerField()
     rating = models.FloatField(default=0.0)
+
+    # New fields for rating
+    rating_count = models.PositiveIntegerField(default=0)
+    rating_total = models.PositiveIntegerField(default=0)
+
+    def average_rating(self):
+        return self.rating_total / self.rating_count if self.rating_count > 0 else 0
 
     class Meta:
         ordering = ('title',)
@@ -29,4 +37,14 @@ class BookCopy(models.Model):
             return f'{self.book.title}, {str(self.borrow_date)}'
         else:
             return f'{self.book.title} - Available'
+
+
+class BookRating(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    rated_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('book', 'user')  # A user can rate a book only once
 
